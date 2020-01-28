75a7ccaca500e2319e76cbe3fe9e7c322062b7da
Isac Kim <jk724814@gmail.com>
2020-01-28 16:53:25 +0000

    FEAT: added seeds, added map, markers, and displayed them

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,11 @@
 class ActivitiesController < ApplicationController
-  def show
-    @activities = Activity.all
+  def index
+    @activities = Activity.where.not(latitude: nil, longitude: nil)
+    @markers = @activities.map do |activity|
+      {
+        lng: activity.longitude,
+        lat: activity.latitude
+      }
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p "destroying all activities"
+Activity.all.destroy_all
+
+p "creating activitie"
+Activity.create(name: "American School of Guayaquil", category: "education", task: "Graduate high-school with a focus in Mathematics and Physics", address: "5, Av. Juan Tanca Marengo, Guayaquil")
+Activity.create(name: "Hanyang University", category: "education", task: "Major in Business Administration: transferred to Ohio University after 3 1/2 years", address: " 222 Wangsimni-ro, Sageun-dong, Seongdong-gu, Seoul")
+Activity.create(name: "Ohio University", category: "education", task: "Major in Marketing Media with a Minor in Business Administration", address: " Athens, OH 45701")
+Activity.create(name: "Le Wagon", category: "education", task: "A 9 week Coding Bootcamp to become a Full-stack Developer", address: "Carrer d'en Grassot, 101, 08025 Barcelona")
+
+
+Activity.create(name: "Importadora Sam", category: "work", task: "Sales Manager at Importadora Sam", address:"Olmedo 233 y Chile, guayaquil")
+Activity.create(name: "Disney English", category: "work", task: "Foreing Trainer: Taught English and trained new incoming Foreign Trainers", address: "90 Guangqumen Inner St, Dongcheng Qu, Beijing Shi", latitude: 39.892808, longitude: 116.421335)
+Activity.create(name: "TOPDOX", category: "work", task: "Digital Marketing Intern: scheduled media posts, focused on email marketing, and created content such as videos and blogposts", address:"4200-804 Porto")
+
+p "#{Activity.count} Activities have been created"

97b9e4094c7cc3cd6ad4581929ce78ccd7d9fe29
Isac Kim <jk724814@gmail.com>
2020-02-07 16:18:24 +0000

    FEAT:Styled and added Slick for the slide

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,45 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import $ from 'jquery';
+
+import 'slick-carousel';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import { initMapbox } from './map';
 initMapbox();
+
+
+$('.slider').slick({
+  dots: true,
+  speed: 100,
+});
+// $(".slider").slick({
+
+//   // normal options...
+//   infinite: false,
+
+//   // the magic
+//   responsive: [{
+
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         infinite: true
+//       }
+
+//     }, {
+
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2,
+//         dots: true
+//       }
+
+//     }, {
+
+//       breakpoint: 300,
+//       settings: "unslick" // destroys slick
+
+//     }]
+// });
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_30_142924) do
+ActiveRecord::Schema.define(version: 2020_02_06_125106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2020_01_30_142924) do
     t.float "longitude"
     t.integer "year"
     t.string "country"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
